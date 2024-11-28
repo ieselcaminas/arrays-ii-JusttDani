6bce90f6f7083aa1d0080529b9f7cd70539e1055
JusttDani <danitejedormartinez@gmail.com>
2024-11-28 11:49:28 +0100

    Update Comunes.java

--- src/main/Comunes.java
+++ src/main/Comunes.java
@@ -1,5 +1,20 @@
-public class Comunes {
-    public static void main(String[] args) {
+public class comunes {
+        public static void main(String[] args) {
+            int[] numeros = {2,4,6,10};
+            int[] numeros2 = {2,4,7,9};
+            System.out.println(comunes(numeros, numeros2));
 
-    }
+        }
+        public static String comunes(int[] a, int[] b){
+            String resultado = "";
+            for(int i=0; i<a.length; i++){
+                for(int j=0; j<a.length; j++){
+                    if(a[i] == b[j]){
+                        resultado += b[j] + " ";
+                    }
+
+                }
             }
+            return resultado;
+        }
+}
